d53a8a9cd956f3042870094825dcfab66c5ec1b9
thales-goncalves3 <thalesgoncalves3@gmail.com>
2022-12-21 20:26:50 -0300

    adicionando algumas funções para o jogo

--- script.js
+++ script.js
@@ -1,62 +1,111 @@
 const url = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
 
 
-getDeck();
+startGame();
 
-async function getDeck(){
+async function startGame() {
+    const id = await getId();
 
-    const id =  await getId();
+    getDeck(id);
 
-    getCardsCom(id);
-    getCardsPlayer(id);
+    const buttonBuyCard = document.getElementById('buy');
+    buttonBuyCard.addEventListener('click', async () => {
+        const card = await getCards(id, 1);
+
+        const divPlayer = document.getElementById('player');
+        const image = document.createElement('img');
+        image.src = card[0]['image'];
+        console.log(card[0]);
+
+        divPlayer.appendChild(image);
+    });
+
+}
+
+
+async function getDeck(id) {
+
+    showSum('countCom', await getCardsCom(id));
+    showSum('countPlayer', await getCardsPlayer(id));
+
 
 }
 
 
 
-async function getCardsCom(id){
-    console.log(id);
+async function getCardsCom(id) {
+
     const com = document.getElementById("com");
-
-    const cards = await getCards(id);
+    var values = [];
+    const cards = await getCards(id, 2);
 
     cards.forEach(element => {
         const image = document.createElement("img");
-
+        values.push(element['value']);
         image.src = element['image'];
 
         com.appendChild(image);
     });
-    
+
+    return values;
+
 }
 
-async function getCardsPlayer(id){
-    console.log(id);
+async function getCardsPlayer(id) {
+
     const com = document.getElementById("player");
-
-    const cards = await getCards(id);
+    var values = [];
+    const cards = await getCards(id, 2);
 
     cards.forEach(element => {
         const image = document.createElement("img");
-
+        values.push(element['value']);
         image.src = element['image'];
 
         com.appendChild(image);
     });
-    
+
+    return values;
 }
 
-async function getCards(id){
-    const response = await fetch(`https://deckofcardsapi.com/api/deck/${id}/draw/?count=2`);
+async function getCards(id, amount) {
+    const response = await fetch(`https://deckofcardsapi.com/api/deck/${id}/draw/?count=${amount}`);
     const cards = await response.json();
 
     return cards['cards'];
 }
 
 
-async function getId(){
+async function getId() {
     const response = await fetch(url);
     const data = await response.json();
 
     return data['deck_id'];
+}
+
+
+async function showSum(player, values) {
+    const scoreboard = document.getElementById(player);
+
+
+    const special = ['KING', 'QUEEN', 'JACK', 'ACE'];
+    var sum = 0;
+
+    values.forEach((element, index) => {
+        if (index == 0 && element == 'ACE') {
+            sum += 11;
+
+        } else {
+            if (special.includes(element)) {
+                sum += 10;
+            } else {
+
+                sum += parseInt(element);
+            }
+        }
+
+    })
+
+    scoreboard.innerHTML = sum;
+
 }
